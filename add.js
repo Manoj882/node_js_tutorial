31958392c2d2ed4e39c6e449df98c034c0fb5b0b
Manoj882 <manojbk488@gmail.com>
2024-01-15 21:39:33 +0545

    module exports

--- add.js
+++ add.js
@@ -1,5 +1,3 @@
 const add = (a, b) => a + b;
 
-const sum = add(4, 5);
-
-console.log(sum);
+module.exports = add;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 // load or import the file add.js
-
-require('./add.js');
+ 
+const addFunc = require('./add.js');
 
 console.log('Hello world from index.js');
+
+const sum1 = addFunc(3,4);
+const sum2 = addFunc(7.5,5.5);
+
+console.log(sum1);
+console.log(sum2);
