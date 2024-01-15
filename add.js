36bea1be25c21ab82dd29ea4e9fd61041e9e8463
Manoj882 <manojbk488@gmail.com>
2024-01-15 21:36:00 +0545

    create add function and it is loaded using require function in index.js

---
+++ add.js
@@ -0,0 +1,5 @@
+const add = (a, b) => a + b;
+
+const sum = add(4, 5);
+
+console.log(sum);
---
+++ index.js
@@ -0,0 +1,6 @@
+
+// load or import the file add.js
+
+require('./add.js');
+
+console.log('Hello world from index.js');
