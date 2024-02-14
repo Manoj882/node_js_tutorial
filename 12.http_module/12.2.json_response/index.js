04f6076d2a2be2ab70a65db768e3deeec7bafc3b
Manoj882 <manojbk488@gmail.com>
2024-02-14 08:58:43 +0545

    move the file

---
+++ 12.http_module/12.2.json_response/index.js
@@ -0,0 +1,16 @@
+
+
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+    const superHero = {
+        "firstName" : "Bruce",
+        "lastName" : "Wayne"
+    };
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(superHero));
+});
+
+server.listen(3000, () => {
+    console.log("Seerver running on port number 3000...");
+});
